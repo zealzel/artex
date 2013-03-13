3f2828e7aa58a7cc4e0aabf620cc4ae70991d39a
Kevin Lee <zealzel@iMac.local>
2013-03-14 00:24:01 +0800

    artex ready for heroku push

--- artexCRM/settings.py
+++ artexCRM/settings.py
@@ -7,14 +7,9 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
-EMAIL_USE_TLS = False
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER = 'zealzel@gmail.com'
-EMAIL_HOST_PASSWORD = 'lisztpower'
-EMAIL_PORT = 587
-
 MANAGERS = ADMINS
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -25,6 +20,15 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+'''
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES={}
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
@@ -34,11 +38,13 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Taipei'
+
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-tw'
 
 SITE_ID = 1
 
